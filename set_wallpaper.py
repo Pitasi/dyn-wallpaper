8ead47a141d1dda56442d42a096b313492b241fa
Antonio Pitasi <pitasi.antonio@gmail.com>
2018-08-16 15:04:30 +0200

    Fix wrong images array index

--- set_wallpaper.py
+++ set_wallpaper.py
@@ -108,7 +108,7 @@ def get_current_images(dawn_time, day_length, images, dusk_id):
     now = datetime.now(tzlocal())
     cursor = (now - dawn_time).total_seconds()
     image_id = dusk_id * cursor / day_length
-    if image_id < 1 or image_id > len(images) - 1:
+    if image_id < 0 or image_id > len(images) - 1:
         # out of range, just pick last image
         last_image = images[len(images) - 1]
         return (last_image, last_image, 1)
